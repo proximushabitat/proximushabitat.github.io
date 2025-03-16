9143bdd2da1177955bbc753b9bc89fd51225120b
Johann Keller <jkjoehu88@gmail.com>
2025-03-16 23:15:20 +0300

    chuuuumSchoooo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+//to have github pages resolve the aliases
+import { path } from 'path'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
@@ -23,4 +26,23 @@ export default defineConfig({
     redirects: true,
   },
   site: 'https://proximushabitat.github.io',
+  /*vite: {
+    resolve: {
+      alias: {
+        // your aliases should mirror the ones defined in your tsconfig.json
+        // i'll re-use the ones in the official docs:
+        //"@components": path.resolve(path.dirname(''), './src/components'),
+
+        "@": path.resolve(path.dirname(''), './src'),
+        "@components": path.resolve(path.dirname(''), './src/components'),
+        "@assets": path.resolve(path.dirname(''), './src/assets'),
+        "@icons": path.resolve(path.dirname(''), './src/icons'),
+        "@layouts": path.resolve(path.dirname(''), './src/layouts'),
+        "@style": path.resolve(path.dirname(''), './/src/style'),
+        "@lib": path.resolve(path.dirname(''), './/src/lib'),
+        "@i18n": path.resolve(path.dirname(''), './/src/i18n'),
+        "@snippets": path.resolve(path.dirname(''), './/src/snippets'),
+      }
+    }
+  }*/
 });

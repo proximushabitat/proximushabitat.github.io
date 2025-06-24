001a8de3976fc33f3f1d7625ed7dcc3d82e117c3
Johann Keller <jkjoehu88@gmail.com>
2025-06-24 22:16:56 +0200

    mehr abstand im schwarzen

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
-["src/collections/sections/de/general.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fgeneral.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/de/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2FintroText01.mdx&astroContentModuleFlag=true")],
 ["src/collections/sections/de/news.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fnews.mdx&astroContentModuleFlag=true")],
+["src/collections/sections/de/general.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fgeneral.mdx&astroContentModuleFlag=true")],
 ["src/collections/news/de/001-fischermaetteli-fachbericht.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnews%2Fde%2F001-fischermaetteli-fachbericht.mdx&astroContentModuleFlag=true")],
+["src/collections/bigBits/de/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2FintroText01.mdx&astroContentModuleFlag=true")],
 ["src/collections/news/de/news-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnews%2Fde%2Fnews-1.mdx&astroContentModuleFlag=true")],
 ["src/collections/news/de/002-astra-zahlenspiele.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnews%2Fde%2F002-astra-zahlenspiele.mdx&astroContentModuleFlag=true")],
 ["src/collections/general/de/dse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fgeneral%2Fde%2Fdse.mdx&astroContentModuleFlag=true")]]);
